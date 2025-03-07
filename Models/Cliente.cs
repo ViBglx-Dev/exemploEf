86158955dd9931c72447fd7b902aad86aba36da6
ViBglx-Dev <vitoria.bougleux@aluno.senai.br>
2025-03-07 10:28:42 -0300

    Adicionando atividade ao GitHub

---
+++ Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace ExemploEF.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+
+    }
+}
